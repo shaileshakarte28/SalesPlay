1b4578904170f14c8d8490bb4f9b273f363e43b0
Your Name <you@example.comshaileshakarte28@gmail.com>
2020-12-17 15:01:03 +0530

    Some Changes

--- SalesPlayProject/settings.py
+++ SalesPlayProject/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
 
 # Quick-start development settings - unsuitable for production
